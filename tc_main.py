0640e03e76b8ea97b38c3a0c3f4da4390a3ebd15
benjamin.dalmas <benjamindalmas@gmail.com>
2023-12-26 12:56:23 +0100

    ajout front main

--- tc_main.py
+++ tc_main.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
-st.title("My first app")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import openai
+from openai import OpenAI
+
+st.title("Génère ton plan de cours !")
+
+niveau = st.selectbox(
+    'Quel niveau ?',
+    ('CP', 'CE1', 'CE2', 'CM1', 'CM2'))
+
+duree = st.selectbox(
+    'Quelle durée ?',
+    ('1H', '2H', '1J', '2J', '5J'))
+
+theme = st.selectbox(
+    'Quel thème ?',
+    ('Les dinosaures', 'Les pharaons', 'La grèce antique'))
+
+matière = st.selectbox(
+    'Quelle matière ?',
+    ('Maths - Additions/soustraction', 'Français - Compréhension de texte', 'Culture générale'))
+
+res = st.button("Générer", key=None, help=None,args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
